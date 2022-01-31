a1fb41cc0bc4ae01eebfe0d9b93f3200575d6ba2
Peter Marsh <breadloaf64@gmail.com>
2022-01-31 19:19:55 +0000

    updated test sketch

--- Pages/FinalYearProject/Sketches/Test/mySketch.js
+++ Pages/FinalYearProject/Sketches/Test/mySketch.js
@@ -1,56 +1,22 @@
-let capture;
-let facingUser = true;
-
 function setup() {
-  createMetaTag();
-  createCanvas(window.innerWidth, window.innerHeight);
-  
-  pixelDensity(1);
-  
-  setupCamera();
-  
-  frameRate(60);
+	createMetaTag();
+	createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
-  if (facingUser) {
-    // flip camera horizontally
-    translate(width, 0);
-    scale(-1, 1);
-  }
-  image(capture, 0, 0, width, height);
-}
-
-function touchStarted() {
-  facingUser = !facingUser;
-  setupCamera();
-}
-
-function setupCamera() {
-  if (capture) {
-    capture.remove();
-  }
-  
-  capture = createCapture({
-    video: {
-      facingMode: facingUser ? 'user' : 'environment'
-    },
-    audio: false
-  });
-  capture.size(width, height);
-  capture.hide();
-}
-
-function touchMoved() {
-  // prevent the display from moving around when you touch it
-  return false;
+	background(map(mouseX, 0, width, 0, 255));
+	fill(255);
+	ellipse(width / 2, height / 2, 100, 100);
+    fill(255, 0, 0);
+    text("window.innerWidth: " + window.innerWidth, 100, 100);
+    text("window.innerHeight: " + window.innerHeight, 100, 100);
 }
 
 function createMetaTag() {
-  let meta = createElement('meta');
-  meta.attribute('name', 'viewport');
-  meta.attribute('content', 'user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,height=device-height');
-  
-  let head = select('head');
-  meta.parent(head);
+	let meta = createElement('meta');
+	meta.attribute('name', 'viewport');
+	meta.attribute('content', 'user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width,height=device-height');
+
+	let head = select('head');
+	meta.parent(head);
 }
