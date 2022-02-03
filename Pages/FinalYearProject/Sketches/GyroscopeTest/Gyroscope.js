d1e605cc8fb57bf147efe9c16413b58ece6d5b88
Peter Marsh <breadloaf64@gmail.com>
2022-02-03 15:04:45 +0000

    changed default gyro values for debug

--- Pages/FinalYearProject/Sketches/GyroscopeTest/Gyroscope.js
+++ Pages/FinalYearProject/Sketches/GyroscopeTest/Gyroscope.js
@@ -1,14 +1,14 @@
-var gyroAlpha = 0, gyroBeta = 0, gyroGamma = 0;
+var gyroAlpha = 1, gyroBeta = 1, gyroGamma = 1;
 var isMobile = false;
 
 // accelerometer Data
 window.addEventListener('deviceorientation', function(e) {
-	
+
 	// if value changed and not 0, then the device has a gryoscope, therefore is mobile
 	if (gyroBeta != e.beta && gyroBeta != 0) {
 		isMobile = true;
 	}
-	
+
   gyroAlpha = e.alpha;
   gyroBeta = e.beta;
   gyroGamma = e.gamma;
