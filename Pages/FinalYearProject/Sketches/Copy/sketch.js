749158b21eb0c2f0ca4579c6e22615ebe0e5b9dd
Peter Marsh <breadloaf64@gmail.com>
2022-02-03 16:26:37 +0000

    reinstated copy wesite

--- Pages/FinalYearProject/Sketches/Copy/sketch.js
+++ Pages/FinalYearProject/Sketches/Copy/sketch.js
@@ -1,65 +1,47 @@
+var alpha, beta, gamma;
+var xpos, ypos;
 
-let permissionGranted = false;
-let cx, cy;
+function setup()
+{
+  // set canvas size
+  var myCanvas = createCanvas(400, 400);
+  myCanvas.parent('sketch');
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  cx = width/2;
-  cy = height/2;
-
-  // DeviceOrientationEvent, DeviceMotionEvent
-  if (typeof(DeviceOrientationEvent) !== 'undefined' && typeof(DeviceOrientationEvent.requestPermission) === 'function') {
-    // ios 13 device
-
-    DeviceOrientationEvent.requestPermission()
-      .catch(() => {
-        // show permission dialog only the first time
-        let button = createButton("click to allow access to sensors");
-        button.style("font-size", "24px");
-        button.center();
-        button.mousePressed( requestAccess );
-        throw error;
-      })
-      .then(() => {
-        // on any subsequent visits
-        permissionGranted = true;
-      })
-  } else {
-    // non ios 13 device
-    textSize(48);
-    // text("non ios 13 device", 100, 100);
-    permissionGranted = true;
-  }
+  // default values
+  xpos = 200;
+  ypos = 200;
+  alpha = 0;
+  beta = 0;
+  gamma = 0;
 }
 
-function requestAccess() {
-  DeviceOrientationEvent.requestPermission()
-    .then(response => {
-      if (response == 'granted') {
-        permissionGranted = true;
-      } else {
-        permissionGranted = false;
-      }
-    })
-  .catch(console.error);
+function draw()
+{
+  // set background color to white
+  background(255);
 
-  this.remove();
+  // display variables
+  fill(100);
+  noStroke();
+  text("alpha: " + alpha, 25, 25);
+  text("beta: " + beta, 25, 50);
+  fill(255, 0, 0);
+  text("gamma: " + gamma, 25, 75);
+
+  push();
+  translate(200, 200);
+  rotate(radians(gamma));
+  fill(255, 0, 0);
+  rect(-40, -40, 80, 80);
+  fill(0);
+  rect(-40, 40, 80, 5);
+  pop();
 }
 
-function draw() {
-  if (!permissionGranted) return;
-
-  // background(255);
-
-  // rotationX, rotationY
-  const dx = constrain(rotationY, -3, 3);
-  const dy = constrain(rotationX, -3, 3);
-  cx += dx*2;
-  cy += dy*2;
-  cx = constrain(cx, 0, width);
-  cy = constrain(cy, 0, height);
-
-  ellipse(cx, cy, 200, 200);
-
-}
+// accelerometer Data
+window.addEventListener('deviceorientation', function(e)
+{
+  alpha = e.alpha;
+  beta = e.beta;
+  gamma = e.gamma;
+});
