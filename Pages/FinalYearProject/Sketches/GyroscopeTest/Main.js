211b8c8cc2d4e626e7b8c571c1c305e3b341b68e
Peter Marsh <breadloaf64@gmail.com>
2022-02-03 14:38:20 +0000

    added gyroscope test

---
+++ Pages/FinalYearProject/Sketches/GyroscopeTest/Main.js
@@ -0,0 +1,51 @@
+var counter = 0;
+
+var currentMouseX = 0;
+var currentMouseY = 0;
+
+var btnPause;
+var paused = true;
+
+function preload() {
+	
+}
+
+function setup() {
+	myStandardSetup();
+	makePauseButton();
+}
+
+function keyPressed() {
+	if (key == ' ' || key == 'p'){ //spacebar or p to pause/unpause
+    pauseUnpause();
+  } 
+	return false;
+}
+
+function pauseUnpause() {
+	paused = !paused;
+	if (!paused) {
+		
+	}
+	else {
+		
+	}
+}
+
+function mouseClicked() {
+	if (paused) pauseUnpause();
+	btnPause.tryClick();
+}
+
+function handleCurrentMouse() {
+	if (!paused) {
+		currentMouseX = mouseX;
+		currentMouseY = mouseY;
+	}
+}
+
+function draw() {
+	handleCurrentMouse();
+	render();
+	counter++;
+}
