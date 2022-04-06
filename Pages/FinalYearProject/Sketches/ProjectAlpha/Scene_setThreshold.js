b30c4f0d492a56dab9b84a96927bfc825b783a39
Peter Marsh <breadloaf64@gmail.com>
2022-04-06 17:37:24 +0100

    test filter on debug image

--- Pages/FinalYearProject/Sketches/ProjectAlpha/Scene_setThreshold.js
+++ Pages/FinalYearProject/Sketches/ProjectAlpha/Scene_setThreshold.js
@@ -22,6 +22,7 @@ class Scene_setThreshold {
 		let filtered = copyImage(photo);
 		//filtered.filter(THRESHOLD, 0.5);
 		//filtered.filter(GRAY);
+		debugimg.filter(GRAY);
 		drawImage(debugimg, capSquareX, capSquareY, capSquareL, capSquareL);
 		this.btnRetakePhoto.draw();
 		this.btnConvertToSequence.draw();
