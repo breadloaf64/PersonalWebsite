04d9a6ef96390bf7f31c20fbd49debdd3d7205b6
Peter Marsh <breadloaf64@gmail.com>
2022-04-06 17:11:06 +0100

    fixed the touchend click for imagefilters

--- Pages/FinalYearProject/Sketches/ImageFilters/Main.js
+++ Pages/FinalYearProject/Sketches/ImageFilters/Main.js
@@ -37,18 +37,24 @@ function mouseClicked() {
 	click();
 }
 
+var timeAtTouchStart = 0;
 function touchStarted() {
 	if (embeddedOnWebsite && mouseIsOnScreen()) {
 		// if on a website, don't register the click if it's off the sketch
 		return false;
 	}
+	timeAtTouchStart = millis();
 }
 
 function touchEnded() {
 	if (clickedThisFrame) return false;
 
-	clickedThisFrame = true;
-	click();
+	// check for just a quick tap
+	if (millis() - timeAtTouchStart < 500) {
+		clickedThisFrame = true;
+		click();
+	}
+
 }
 
 function click() {
