655da3965b2aa1779c2528d4f593e5c408b0471e
Peter Marsh <pmarsh@scottlogic.com>
2024-04-10 16:02:48 +0100

    replace all original website with Astro template

--- hidden-pages/mothers-day-2022/Colours.js
+++
@@ -1,7 +0,0 @@
-function setColours() {
-    colBackground = color('#ffddd2')
-    colFlowerPetals = color('#e29578')
-    colFlowerMiddle = color('#ffddd2')
-    colMessage = color('#006d77')
-    colVines = color('#FFF0EB')
-}
