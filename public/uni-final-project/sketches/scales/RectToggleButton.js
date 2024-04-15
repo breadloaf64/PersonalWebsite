5cb7c4c6bd3af6a6aeeab5a5f867a311c6bb0069
Peter Marsh <pmarsh@scottlogic.com>
2024-04-15 10:26:16 +0100

    adds scales sketch

---
+++ public/uni-final-project/sketches/scales/RectToggleButton.js
@@ -0,0 +1,54 @@
+class RectToggleButton {
+    constructor(x, y, w, h, text) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.text = text
+
+        this.state = true
+    }
+
+    setSize(x, y, w, h) {
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+    }
+
+    tryClick() {
+        // check if mouse is over the button
+        if (
+            this.x < mouseX &&
+            mouseX < this.x + this.w &&
+            this.y < mouseY &&
+            mouseY < this.y + this.h
+        ) {
+            this.click()
+        }
+    }
+
+    click() {
+        this.state = !this.state
+    }
+
+    draw() {
+        // draw button
+        strokeWeight(5)
+        stroke(colDegreeButtonBorder)
+        if (this.state) fill(colDegreeButtonFillOn)
+        else fill(colDegreeButtonFillOff)
+
+        rect(this.x, this.y, this.w, this.h)
+
+        //draw text
+        noStroke()
+        fill(colDegreeButtonBorder)
+
+        text(
+            this.text,
+            this.x + (this.w - textWidth(this.text)) / 2,
+            this.y + this.h / 2 + 0.25 * textSize()
+        )
+    }
+}
